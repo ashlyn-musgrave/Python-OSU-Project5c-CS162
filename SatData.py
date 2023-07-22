2eaa8447c963530fabb9fd7931e45e452cd98661
ashlyn-musgrave <102622407+ashlyn-musgrave@users.noreply.github.com>
2023-07-21 22:49:27 -0500

    7/21/23 - This is the second submission for assignment 5c

--- SatData.py
+++ SatData.py
@@ -5,35 +5,37 @@
 # City and writes the data to a text file in CSV format
 
 import json
+
 class SatData:
     """This class reads a JSON file containing data on 2010 SAT results for New York City and
     writes the data to a text file in CSV format"""
 
     def __init__(self):
         """This method reads the file and stores it in a data method"""
-        with open('sat.json', 'r') as f:
-            self._data = json.load(f)['data']
+        with open('sat.json', 'r') as file:
+            self.data = json.load(file)
 
-    def save_as_csv(self, dbns):
+    def save_as_csv(self, dbn_list):
         """This method takes as a parameter a list of DBNs (district bureau numbers) and saves a
         CSV file that looks like this, but with only the rows that correspond to the DBNs in the
         list"""
-        sat_list = []
-        for data in self._data:
-            if data[8] in dbns:
-                sat_list.append(data)
+        rows = []
+        headers = ['DBN', 'School Name', 'Number of Test Takers', 'Critical Reading Mean', 'Mathematics Mean', 'Writing Mean']
 
-        with open('output.csv', 'w+') as outfile:
-            header = [str(i) for i in range(len(sat_list[3]))]
-            outfile.write
-            for row in sat_list:
-                sat_info = []
-                for item in row:
-                    if ',' in str(item):
-                        sat_info.append("\"" + item + "\"")
-                    else:
-                        sat_info.append(str(item))
-                outfile.write(','.join(sat_info))
-                outfile.write('\n')
-        outfile.close()
+        for school_data in self.data:
+            if school_data['DBN'] in dbn_list:
+                row = []
+                for header in headers:
+                    value = school_data[header]
+                    if ',' in value:
+                        value = '"' + value + '"'
+                    row.append(value)
+                rows.append(row)
 
+        rows = sorted(rows, key=lambda x: x[0])
+
+        with open('output.csv', 'w') as f:
+            f.write(','.join(headers) + '\n')
+
+            for row in rows:
+                f.write(','.join(row) + '\n')
